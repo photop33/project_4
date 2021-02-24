80cebc648e458b4d04dcb413e4777c2637c33fff
photop33 <75907575+photop33@users.noreply.github.com>
2021-02-25 00:32:56 +0200

    Update docker_backend_testing.py

---
+++ docker_backend_testing.py
@@ -0,0 +1,14 @@
+import requests
+user_id= "5"
+user_name= 'Shola'
+
+res = requests.post('http://127.0.0.1:5000/user/'+ user_id +'', json={"user_name": ""+ user_name +""})
+if res.ok:
+    print(res.json())
+
+
+
+res = requests.get('http://127.0.0.1:5000/user/'+ user_id  +'')
+if res.ok:
+    print(res.json())
+    
