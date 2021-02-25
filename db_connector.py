4361262804dfac205f7bc885f1302c9a3ffc8f1c
photop33 <75907575+photop33@users.noreply.github.com>
2021-02-25 18:18:34 +0200

    Update db_connector.py

--- db_connector.py
+++ db_connector.py
@@ -1,38 +1,41 @@
 import pymysql
 import datetime
-from flask import  request
-
+from flask import request
 
 
 def Create_Table():
     conn = pymysql.connect(host='remotemysql.com', port=3306, user='Q2PbjAC1nT', passwd='WRYn22HLYY', db='Q2PbjAC1nT')
     cursor = conn.cursor()
     conn.autocommit(True)
-    name_table="user"
-    cursor.execute("CREATE TABLE `Q2PbjAC1nT`.`"+ name_table +"` (`ID` INT UNSIGNED NOT NULL,`name` VARCHAR(50) NOT NULL,""`time_column`datetime (`ID`))")
-    print("Ready table "+ name_table +"")
+    name_table = "user"
+    cursor.execute(
+        "CREATE TABLE `Q2PbjAC1nT`.`" + name_table + "` (`ID` INT UNSIGNED NOT NULL,`name` VARCHAR(50) NOT NULL,`time_column_datetime` VARCHAR(50) NOT NULL)")
+    print("Ready table " + name_table + "")
     cursor.close()
     conn.close()
-
 
 
 def get_users(user_id):
     conn = pymysql.connect(host='remotemysql.com', port=3306, user='Q2PbjAC1nT', passwd='WRYn22HLYY', db='Q2PbjAC1nT')
     cursor = conn.cursor()
     conn.autocommit(True)
-    x=cursor.execute("SELECT * FROM Q2PbjAC1nT.users;")
+    x = cursor.execute("SELECT * FROM Q2PbjAC1nT.users;")
     result = cursor.fetchall()
     for row in result:
-        show = str(row[0])
+        show = str(row[1])
         if show == user_id:
-            user_name = row[1]
+            user_name = row[0]
+            print("success get_users")
             return user_name
+        else:
+            print('Fail get_user')
     cursor.close()
     conn.close()
 
-
 users = {}
-def insert_user(user_id):
+
+
+def insert_user(user_id) :
     request_data = request.json
     user_name = request_data.get('user_name')
     users[user_id] = user_name
@@ -41,12 +44,18 @@ def insert_user(user_id):
     cursor = conn.cursor()
     now = datetime.datetime.utcnow()
     str_now = now.date().isoformat()
-    cursor.execute('INSERT INTO Q2PbjAC1nT.users (name, user_id, time_column) VALUES (%s,%s,%s)',(user_name, user_id, str_now))
+    cursor.execute("INSERT INTO Q2PbjAC1nT.users (`name`, `user_id` ,time ) VALUES (%s,%s,%s)", (user_name, user_id,str_now))
+    # `time_column`
     cursor.close()
     conn.close()
-    print("success")
+    print("success insert_user")
+    return user_name
+
+
 
 users = {}
+
+
 def update_user(user_id):
     request_data = request.json
     request_data.get('user_name')
@@ -58,7 +67,10 @@ def update_user(user_id):
     cursor.execute("UPDATE Q2PbjAC1nT.users SET name = '" + user_name + "'  WHERE user_id=" + user_id + "")
     cursor.close()
     conn.close()
+    print("success update_user")
+
     return user_name
+
 
 def delete_user(user_id):
     conn = pymysql.connect(host='remotemysql.com', port=3306, user='Q2PbjAC1nT', passwd='WRYn22HLYY', db='Q2PbjAC1nT')
@@ -74,4 +86,8 @@ def delete_user(user_id):
     cursor.execute("DELETE FROM Q2PbjAC1nT.users WHERE user_id = " + user_id + "")
     cursor.close()
     conn.close()
-    return 
+    print("success delete_user")
+    return user_name
+
+
+
