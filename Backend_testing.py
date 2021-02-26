0d4ed2d746c7f306e42ccdc01a16efad3f62bae9
photop33 <75907575+photop33@users.noreply.github.com>
2021-02-26 21:00:59 +0200

    Update Backend_testing.py

--- Backend_testing.py
+++ Backend_testing.py
@@ -1,6 +1,6 @@
 import requests
-user_id= "5"
-user_name= 'Shola'
+user_id= "8"
+user_name= 'dad'
 
 res = requests.get('http://127.0.0.1:5000/user/'+ user_id +'', json={"user_name": ""+ user_name +""})
 if res.ok:
@@ -14,13 +14,13 @@ if res.ok:
     
     
     
-res = requests.put('http://127.0.0.1:5000/user/'+ user_id  +'')
-if res.ok:
-    print(res.json())
+# res = requests.put('http://127.0.0.1:5000/user/'+ user_id  +'')
+# if res.ok:
+#     print(res.json())
     
     
     
     
-res = requests.delete('http://127.0.0.1:5000/user/'+ user_id  +'')
-if res.ok:
-    print(res.json())
+# res = requests.delete('http://127.0.0.1:5000/user/'+ user_id  +'')
+# if res.ok:
+#     print(res.json())
