302232ec7276a757cd37a983d06a782275eac08f
photop33 <75907575+photop33@users.noreply.github.com>
2021-02-27 23:55:57 +0200

    Update docker_backend_testing.py

--- docker_backend_testing.py
+++ docker_backend_testing.py
@@ -7,6 +7,15 @@ user_name = 'lior'
 res = requests.get('http://127.0.0.1:5005/user/' + user_id + '')
 if res.ok:
     print(res.json())
-
-x = db_connector.get_users(user_id)
-print(x)
+    
+conn = pymysql.connect(host='remotemysql.com', port=3306, user='Q2PbjAC1nT', passwd='WRYn22HLYY', db='Q2PbjAC1nT')
+cursor = conn.cursor()
+conn.autocommit(True)
+x = cursor.execute("SELECT * FROM Q2PbjAC1nT.users;")
+result = cursor.fetchall()
+for row in result:
+    show = str(row[1])
+    print(show,user_id)
+    if show == user_id:
+        user_name = row[0]
+        print(user_name)
