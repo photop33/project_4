54eab04d94703aa476af02b4ce2db919faaebff1
photop33 <75907575+photop33@users.noreply.github.com>
2021-02-27 22:55:02 +0200

    Update docker_backend_testing.py

--- docker_backend_testing.py
+++ docker_backend_testing.py
@@ -3,7 +3,7 @@ import pymysql
 user_id= "8"
 
 
-res = requests.get('http://127.0.0.1:5000/user/'+ user_id  +'')
+res = requests.get('http://127.0.0.1:5005/user/'+ user_id  +'')
 if res.ok:
     print(res.json())
 
