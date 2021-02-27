be9d258762587c833a2e5ebdee8dbbbb4a5c17ae
photop33 <75907575+photop33@users.noreply.github.com>
2021-02-28 01:14:21 +0200

    Update docker_backend_testing.py

--- docker_backend_testing.py
+++ docker_backend_testing.py
@@ -5,11 +5,11 @@ user_id = "8"
 
     
 
-res = requests.get('http://127.0.0.1:4009/user/'+ user_id  +'')
+res = requests.get('http://127.0.0.1:4999/user/'+ user_id  +'')
 if res.ok:
     print(res.json())
 
-res = requests.get('http://127.0.0.1:5002/user/'+ user_id  +'')
+res = requests.get('http://127.0.0.1:4999/user/'+ user_id  +'')
 if res.ok:
     print(res.json())
 
