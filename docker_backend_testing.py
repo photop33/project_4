ff0b5e6c8360c55c0205acd47b480f0b07a33ecf
photop33 <75907575+photop33@users.noreply.github.com>
2021-02-27 23:52:24 +0200

    Update docker_backend_testing.py

--- docker_backend_testing.py
+++ docker_backend_testing.py
@@ -4,7 +4,7 @@ import db_connector
 user_id = "8"
 user_name = 'lior'
 
-res = requests.get('http://127.0.0.1:5000/user/' + user_id + '')
+res = requests.get('http://127.0.0.1:5002/user/' + user_id + '')
 if res.ok:
     print(res.json())
 
