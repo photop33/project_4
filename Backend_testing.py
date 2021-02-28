a5074f93409ec59cd1dc3eff9573bcff0dbeaf03
photop33 <75907575+photop33@users.noreply.github.com>
2021-02-28 02:18:11 +0200

    Update Backend_testing.py

--- Backend_testing.py
+++ Backend_testing.py
@@ -3,7 +3,7 @@ user_id= "1"
 user_name= 'Daniel'
 
 
-res = requests.post('http::///127.0.0.1:5000/user/'+ user_id +'', json={"user_name": ""+ user_name +""})
+res = requests.post('http://127.0.0.1:5000/user/'+ user_id +'', json={"user_name": ""+ user_name +""})
 if res.ok:
     print(res.json())
     
