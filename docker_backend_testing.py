21253cce6f9577df7c4d5089d6a3f1f9ff0143cb
photop33 <75907575+photop33@users.noreply.github.com>
2021-03-08 13:45:25 +0200

    Update docker_backend_testing.py

--- docker_backend_testing.py
+++ docker_backend_testing.py
@@ -1,6 +1,12 @@
 import requests
 user_id = "1"
 
+
+res = open (k8s_url-test,"r")
+read = res.read()
+print (res)
+
+
 res = requests.get('http://127.0.0.1:5500/user/' + user_id + '')
 if res.ok:
     print(res.json())
