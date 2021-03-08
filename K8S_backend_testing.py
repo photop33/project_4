4643f6f8480f35366ab1a8c5ce5f61edf18c1936
photop33 <75907575+photop33@users.noreply.github.com>
2021-03-08 14:23:09 +0200

    Update K8S_backend_testing.py

--- K8S_backend_testing.py
+++ K8S_backend_testing.py
@@ -1,6 +1,6 @@
 import requests
 user_id = "1"
 
-res = requests.get('http://127.0.0.1:6000/user/' + user_id + '')
+res = requests.get('http://127.0.0.1:5500/user/' + user_id + '')
 if res.ok:
     print(res.json())
